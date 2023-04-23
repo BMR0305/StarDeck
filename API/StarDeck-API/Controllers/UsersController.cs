adcda4a0ccd73ffe5566394104671799114d77fc
Sormocs <sermartbo@gmail.com>
2023-04-22 20:38:33 -0600

    Add DB context

--- API/StarDeck-API/Controllers/UsersController.cs
+++ API/StarDeck-API/Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StarDeck_API.Models;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,6 +9,11 @@ namespace StarDeck_API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        private readonly DBContext context;
+        public UsersController(DBContext context)
+        {
+            this.context = context;
+        }
         // GET: api/<UsersController>
         [HttpGet]
         public IEnumerable<string> Get()
@@ -24,8 +30,20 @@ namespace StarDeck_API.Controllers
 
         // POST api/<UsersController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Route("post")]
+        public dynamic PostUsers([FromBody] Users u)
         {
+            try
+            {
+                context.users.Add(u);
+                context.SaveChanges();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                return BadRequest(ex.Message);
+            }
         }
 
     }
---
+++ API/StarDeck-API/Models/DBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StarDeck_API.Models
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options) { }
+
+        public DbSet<Users> users => Set<Users>();
+    }
+}
