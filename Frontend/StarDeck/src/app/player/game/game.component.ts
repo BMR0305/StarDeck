f0f0134001760aaa812b15ba900dc59a8480efc3
David Richmond Soto <luis.richmond9@gmail.com>
2023-05-30 20:34:14 -0600

    add cards in party

--- Frontend/StarDeck/src/app/player/game/game.component.ts
+++ Frontend/StarDeck/src/app/player/game/game.component.ts
@@ -5,34 +5,33 @@ import { takeWhile } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { ApiService } from 'src/app/shared/api-module/api.service';
 
-
 @Component({
   selector: 'app-game',
   templateUrl: './game.component.html',
   styleUrls: ['./game.component.css']
 })
 
-
-
 export class GameComponent {
 
   temp: any; //temporal variable to save the cards from the api
 
+  //card back image from assets folder
+  backCardImg = "assets/img/card_back.png"
+
   //Profile player image default
   player_img = 'https://previews.123rf.com/images/ylivdesign/ylivdesign1609/ylivdesign160903327/62577801-icono-de-alien-en-estilo-monocromo-negro-sobre-una-ilustraci%C3%B3n-de-vector-de-fondo-blanco.jpg';
-  
+
   //Image for the deck
   deck = 'https://cdn-icons-png.flaticon.com/512/1178/1178933.png?w=740&t=st=1683845874~exp=1683846474~hmac=64a15b662b3545f116f90292fe99ab6f0f3c3036692aa285b3fe8114b3f4d4c3';
-  
+
   //Image for hidden planet
   planet0 = 'https://cdn-icons-png.flaticon.com/512/16/16268.png?w=740&t=st=1684308443~exp=1684309043~hmac=a9aeb0e725b0764136935741b1d73d2c0d70f29d1e1cd5741c88f45efd447747';
-  
+
   planet1? : Planet;
   planet2? : Planet;
   planet3? : string;
   seconds = 20;
   oponente?: Player;
-
 
   card?: Cards;
 
@@ -40,14 +39,19 @@ export class GameComponent {
   deck_cards: Cards[] = [];
   lenghtdeck = this.deck_cards.length;
   energy = 1;
-  
 
+  planet1TopCards: Cards[] = [];
+  planet1BottomCards: Cards[] = [];
+  planet2TopCards: Cards[] = [];
+  planet2BottomCards: Cards[] = [];
+  planet3TopCards: Cards[] = [];
+  planet3BottomCards: Cards[] = [];
 
   constructor(private router: Router, private apiService: ApiService) {  }
 
-
   ngOnInit(): void {
 
+    this.setCardsEmpty();
 
     this.getOponente(localStorage.getItem('oponent')+"");
 
@@ -64,8 +68,8 @@ export class GameComponent {
 
     this.apiService.get(url).subscribe((data) => {
       this.temp = data;
+      console.log(this.temp);
       this.planet2 = this.temp[0]["p_image"];
-      
     });
 
     url = "Planet/get/" + localStorage.getItem('planet3');
@@ -79,8 +83,6 @@ export class GameComponent {
 
     this.setDeck();
 
-   
-    
     const source = interval(1000);
     const timer = source.pipe(takeWhile(() => this.seconds > 0));
 
@@ -92,8 +94,6 @@ export class GameComponent {
 
   setDeck(){
 
-  
-
     let url = "Deck/get/" + localStorage.getItem("deck");
     url = url.replace(/"/g, "");
 
@@ -101,7 +101,6 @@ export class GameComponent {
       this.temp = data;
       this.temp = this.temp["cards"]
       for(let i = 0; i < this.temp.length; i++){
-        console.log()
         const card: Cards = {
           c_image: this.temp[i]["c_image"],
           id: this.temp[i]["ID"],
@@ -113,29 +112,25 @@ export class GameComponent {
           c_status: this.temp[i]["c_status"],
           c_description: this.temp[i]["c_description"]
         }
-        //console.log(card);
         this.deck_cards.push(card);
         this.lenghtdeck = this.deck_cards.length;
       }
       this.deck_cards.sort(Shuffle);
-  
-      for(let i = 0; i < 7; i++){
+
+      for(let i = 0; i < 5; i++){
         let mycard = this.deck_cards.pop();
         if(mycard) {
           this.hand_cards.push(mycard);
         }
         this.lenghtdeck = this.deck_cards.length;
       }
-      
+
     });
 
-
-
-
-    
   }
 
   getOponente(opp: string) {
+
     let url = "Users/get/" + opp;
     url = url.replace(/"/g, "");
 
@@ -151,7 +146,34 @@ export class GameComponent {
     this.router.navigate(['/playerview/start'])
   }
 
+  setCardsEmpty(){
+
+    let card: Cards = {
+      c_image: this.backCardImg,
+      id: undefined,
+      c_name: undefined,
+      battle_pts: undefined,
+      energy: undefined,
+      c_type: undefined,
+      race: undefined,
+      c_status: undefined,
+      c_description: undefined
+    }
+
+    for (let i = 0; i < 5; i++) {
+      this.planet1TopCards.push(card);
+      this.planet1BottomCards.push(card);
+      this.planet2TopCards.push(card);
+      this.planet2BottomCards.push(card);
+      this.planet3TopCards.push(card);
+      this.planet3BottomCards.push(card);
+    }
+
+  }
+
 }
+
+
 
 function Shuffle() {
   return Math.random() - 0.5;
