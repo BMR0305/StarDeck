bb80c12eea3609f51adcaf1850f105b195506cbf
Sormocs <sermartbo@gmail.com>
2023-06-21 02:50:58 -0600

    Databese Reset Scripts

---
+++ DataBase/Drop DB.sql
@@ -0,0 +1,12 @@
+DELETE FROM Deck_Card; --Insert - done
+DELETE FROM Deck; --Insert - done
+DELETE FROM User_card; --Insert - done
+DELETE FROM CardsLeft;
+DELETE FROM CardPlayed;
+DELETE FROM PlayerTurn_Card;
+DELETE FROM Turn;
+DELETE FROM Partida;
+DELETE FROM Users; --Insert - done
+DELETE FROM Planet; --Insert - done
+DELETE FROM Cards; --Insert - done
+DELETE FROM Race; --Insert - done
